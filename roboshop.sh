cfa1a25ff2b2e9848ff6f80d21b63f3962e81cc9
manu <example@gmail.com>
2026-01-31 18:47:23 +0530

    action

--- roboshop.sh
+++ roboshop.sh
@@ -37,7 +37,7 @@ do
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch '
+    --change-batch "
     {
         "Comment": "Updating record",
         "Changes": [
@@ -57,7 +57,7 @@ do
         ]
     }  
 
-    '
+    "
     
     echo "Record updated for $instance"
 
