4e515f70bb5a4a489eabdcc42d66098aea415bf6
manu <example@gmail.com>
2026-02-01 14:26:58 +0530

    action

--- redis.sh
+++ redis.sh
@@ -35,7 +35,7 @@ dnf install redis -y | tee -a $LOGS_FILE
 validate $? "Installing redis"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGS_FILE
 validate $? "Updating redis bind address"
-sed -i 's/^protected-mode yes/protected-mode no/g' /etc/redis.conf &>> $LOGS_FILE
+sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis.conf &>> $LOGS_FILE
 validate $? "Disabling protected mode"
 systemctl enable redis &>> $LOGS_FILE
 systemctl start redis 
