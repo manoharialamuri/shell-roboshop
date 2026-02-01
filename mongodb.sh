fd75a7eed09d400c57408cd4e07b5a9eddd0a8c9
manu <example@gmail.com>
2026-02-01 10:44:13 +0530

    action

---
+++ mongodb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Please use root access" | tee -a $LOGS_FILE
+    exit 12
+fi
+
+mkdir -p $LOGS_FOLDER
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2... Failed" | tee -a $LOGS_FILE
+        exit 30
+    else
+        echo "$2.. Success" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo | tee -a $LOGS_FILE
+validate $? "copying mongo repo"
+
+dnf install mongodb-org -y | tee -a $LOGS_FILE
+validate $? "Installing mongodb server"
+
+systemctl enable mongod 
+systemctl start mongod
+validate $? "Enabling and starting mongodb" 
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+validate $? "Allowing remote users"
+
+systemctl restart mongod
+validate $? "Restarted Mongodb"
