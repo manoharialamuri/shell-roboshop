33246826f05410624c052e562feadc3fea57fee0
manu <example@gmail.com>
2026-02-02 16:50:06 +0530

    action

--- shipping.sh
+++ shipping.sh
@@ -1,4 +1,4 @@
-# 
+
 #!/bin/bash
 
 USERID=$(id -u)
@@ -65,18 +65,10 @@ VALIDATE $? "Created systemctl service"
 
 dnf install mysql -y  &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
-
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
-if [ $? -ne 0 ]; then
-
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
-    VALIDATE $? "Loaded data into MySQL"
-else
-    echo -e "data is already loaded ... $Y SKIPPING $N"
-fi
-
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+VALIDATE $? "Loaded data into MySQL"
 systemctl enable shipping &>>$LOGS_FILE
 systemctl start shipping
 VALIDATE $? "Enabled and started shipping"
