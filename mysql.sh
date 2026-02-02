f22620a3287801b3f76cfce6ddebbd96fad59552
manu <example@gmail.com>
2026-02-02 11:05:10 +0530

    action

---
+++ mysql.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGO_HOST=mongodb.daws88s.store
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Please use root access" | tee -a $LOGS_FILE
+    exit 12
+fi
+
+mkdir -p $LOGS_FOLDER
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2... Failed" | tee -a $LOGS_FILE
+        exit 30
+    else
+        echo "$2.. Success" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y
+validate $? "installing mysql"
+mysql_secure_installation --set-root-pass RoboShop@1
+validate $? "creating password"
+systemctl enable mysqld
+systemctl start mysqld  
+validate $? "Enable and start"
