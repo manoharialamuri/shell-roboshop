7715f47c6443e30313ed538fe688439998018983
manu <example@gmail.com>
2026-02-03 23:40:03 +0530

    changes made

--- redis.sh
+++ redis.sh
@@ -34,7 +34,7 @@ validate $? "Enabling redis 7"
 dnf install redis -y | tee -a $LOGS_FILE
 validate $? "Installing redis"
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-validate $? "Allowing remote connections
+validate $? "Allowing remote connections"
 systemctl daemon-reload
 validate $? "reloaded"
 systemctl enable redis &>> $LOGS_FILE
